bab917c74c541473f62ed1459abc73502a3c1e7f
maysam <Maysssam@gmail.com>
2016-09-16 12:53:42 -0700

    working

--- gulpfile.js
+++ gulpfile.js
@@ -56,8 +56,8 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/css/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch(['*.html','_includes/*.html','_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
