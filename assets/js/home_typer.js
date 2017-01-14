c3c7f002a2a657d67f6f26bf0ad24874c3e7987c
maysam <Maysssam@gmail.com>
2017-01-14 11:54:40 -0800

    maysam is trying

--- assets/js/home_typer.js
+++ assets/js/home_typer.js
@@ -1,10 +1,21 @@
 $('.type-it').typeIt({
-     //speed: 900,
-     lifeLike: false,
-     autoStart: false
+     speed: 130,
+     lifeLike: true,
+     autoStart: true,
 })
-.tiType('maysam is trying to learn,')
+.tiType('Maysam is trying to lre')
+.tiPause(700)
+.tiDelete(6)
+.tiType('to learn, ')
 .tiSettings({speed: 220})
 .tiType('but now ...')
-.tiDelete(11)
-.tiType('but instead ...!');
+.tiDelete(7)
+.tiType('instead')
+.tiPause(600)
+.tiType(' losing!')
+.tiPause(500)
+.tiDelete(7)
+.tiType('spending')
+.tiPause(300)
+.tiDelete(8)
+.tiType('....!?')
