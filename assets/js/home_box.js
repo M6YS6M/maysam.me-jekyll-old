4a7b1745f9e7a209caa2aac709e9638475abc131
maysam <Maysssam@gmail.com>
2017-01-20 14:28:49 -0800

    box

---
+++ assets/js/home_box.js
@@ -0,0 +1,38 @@
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+
+var camera, scene, renderer, w, h;
+var geometry, material, mesh;
+
+
+  var canvas = document.getElementById("myCanvas");
+  var w = canvas.width;
+  var h = canvas.height;
+
+  var renderer = new THREE.WebGLRenderer({canvas: document.getElementById("myCanvas")});
+
+  //renderer.setClearColor(0x00ff00);
+  //renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(w,h);
+
+  var camera = new THREE.PerspectiveCamera(35, w / h, .1, 3000);
+  camera.position.z =100;
+  var scene = new THREE.Scene();
+
+
+  var geometry = new THREE.CubeGeometry(20, 20, 20);
+  var material = new THREE.MeshBasicMaterial({shading: THREE.SmoothShading, color: 0x000000, wireframe: true, wireframeLinewidth: 8, transparent: true, opacity: 1});
+  var mesh = new THREE.Mesh(geometry, material);
+  //mesh.position.y = 100;
+  scene.add(mesh);
+  requestAnimationFrame( render );
+
+function render(){
+  mesh.rotation.x = Date.now() * 0.0008;
+  mesh.rotation.y = Date.now() * 0.0003;
+  mesh.rotation.z = Date.now() * 0.001;
+  renderer.render( scene, camera );
+  requestAnimationFrame( render );
+}
+
+  //controls = new THREE.OrbitControls(camera);
